36c873b4b4331707e97748475ad5dcae950395e6
itsna <itsna@LAPTOP-O6VS6OQ3>
2022-08-30 17:41:43 -0400

    Add project files.

---
+++ Project1MP3/MPThreeDriver.cs
@@ -0,0 +1,21 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+//
+// Project: Project1MP3
+// File Name: MPThreeDriver.cs
+// Description: 
+// Course: CSCI 1260-001 – Introduction to Computer Science II
+// Author: Travis Nagle, naglet@etsu.edu, Department of Computing, East Tennessee State University
+// Created: 08/30/2022
+// Copyright: Travis Nagle, 2022
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+
+public class MPThreeDriver
+{
+    public static void Main()
+    {
+        Console.WriteLine("test");
+    }
+}
